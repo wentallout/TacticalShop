d9e9ce2fc01e832019ff1e68f18cc5b51b2f3e71
Nguyen Dang Khoa <wentallout@gmail.com>
2021-03-28 22:40:48 +0700

    Add Brand and Product models to backend

---
+++ TacticalShop.Backend/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TacticalShop.Backend.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public string ProductImageName { get; set; }
+
+        public int BrandId { get; set; }
+
+        public Brand Brand { get; set; }
+    }
+}
