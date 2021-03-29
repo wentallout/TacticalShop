0be7e0bcc8bd37b8202938a6e1cb4ab1b95030c3
Nguyen Dang Khoa <wentallout@gmail.com>
2021-03-29 17:55:21 +0700

    Create ViewModels for Category

---
+++ TacticalShop.ViewModels/CategoryCreateRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TacticalShop.ViewModels
+{
+    public class CategoryCreateRequest
+    {
+        [Required]
+        [StringLength(30)]
+        public string CategoryName { get; set; }
+    }
+}
---
+++ TacticalShop.ViewModels/CategoryVm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TacticalShop.ViewModels
+{
+    public class CategoryVm
+    {
+        public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string CategoryName { get; set; }
+    }
+}
