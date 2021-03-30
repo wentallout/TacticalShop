f305afb14e87c2a759054e63eef8fe161bd08e48
Nguyen Dang Khoa <wentallout@gmail.com>
2021-03-30 13:05:28 +0700

    Get all scaffold pages from Identity

---
+++ TacticalShop.Backend/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TacticalShop.Backend.Data;
+using TacticalShop.Backend.Models;
+
+[assembly: HostingStartup(typeof(TacticalShop.Backend.Areas.Identity.IdentityHostingStartup))]
+namespace TacticalShop.Backend.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
