92b4763d046ca2a6d11f7f8be6b781abf00c6afb
Nguyen Dang Khoa <wentallout@gmail.com>
2021-03-30 15:24:37 +0700

    Set up viewmodels for Product

---
+++ TacticalShop.ViewModels/ProductCreateRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace TacticalShop.ViewModels
+{
+    public class ProductCreateRequest
+    {
+        
+        [Required]
+        public string ProductName { get; set; }
+
+        public decimal ProductPrice { get; set; }
+
+        public string ProductDescription { get; set; }
+
+            
+      
+
+        public IFormFile ProductImage { get; set; }
+        public int ProductQuantity { get; set; }
+
+        public int BrandId { get; set; }
+        public int CategoryId { get; set; }
+
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        
+
+
+    }
+}
---
+++ TacticalShop.ViewModels/ProductVm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+    namespace TacticalShop.ViewModels
+    {
+        public class ProductVm
+        {
+            public int ProductId { get; set; }
+
+            public string ProductName { get; set; }
+
+            public decimal ProductPrice { get; set; }
+
+            public string ProductDescription { get; set; }
+
+            
+            public string ProductImage { get; set; }
+            public int ProductQuantity { get; set; }
+
+            public DateTime CreatedDate { get; set; }
+
+            public DateTime UpdatedDate { get; set; }
+
+
+        
+
+
+            
+        }
+    }
+    
+
