bfad40988ed80fe4ed30f0424153119bb21f6eb8
Nguyen Dang Khoa <wentallout@gmail.com>
2021-03-31 16:08:05 +0700

    add services for product api

---
+++ TacticalShop.Frontend/Services/IProductApiClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TacticalShop.ViewModels;
+
+namespace TacticalShop.Frontend.Services
+{
+    public interface IProductApiClient
+    {
+        Task<IList<ProductVm>> GetProducts();
+    }
+}
