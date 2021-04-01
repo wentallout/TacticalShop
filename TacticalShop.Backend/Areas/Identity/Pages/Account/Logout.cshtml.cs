766743496081399ef5825546fe2f12299aabdbab
Nguyen Dang Khoa <wentallout@gmail.com>
2021-04-01 12:42:34 +0700

    Make Logout razor file use identityserver

--- TacticalShop.Backend/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ TacticalShop.Backend/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -16,28 +17,48 @@ namespace TacticalShop.Backend.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<User> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
+        private readonly IIdentityServerInteractionService _interaction;
 
-        public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger,IIdentityServerInteractionService interaction)
         {
             _signInManager = signInManager;
             _logger = logger;
+            _interaction = interaction;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGet(string returnUrl = null)
         {
+            return await this.OnPost(returnUrl);
         }
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
+
+            var logoutId = this.Request.Query["logoutId"].ToString();
+
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
             }
+            else if (!string.IsNullOrEmpty(logoutId))
+            {
+                var logoutContext = await this._interaction.GetLogoutContextAsync(logoutId);
+                returnUrl = logoutContext.PostLogoutRedirectUri;
+
+                if (!string.IsNullOrEmpty(returnUrl))
+                {
+                    return this.Redirect(returnUrl);
+                }
+                else
+                {
+                    return Page();
+                }
+            }
             else
             {
-                return RedirectToPage();
+                return Page();
             }
         }
     }
