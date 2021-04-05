9b0cffdaf51464bc3c82df782c59045d07b144f3
Nguyen Dang Khoa <wentallout@gmail.com>
2021-04-05 16:10:15 +0700

    Update Migrations

--- TacticalShop.Backend/Migrations/20210404174332_increaseProductImageNameLength.Designer.cs
+++ TacticalShop.Backend/Migrations/20210404174332_increaseProductImageNameLength.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TacticalShop.Backend.Data;
 
 namespace TacticalShop.Backend.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20210404174332_increaseProductImageNameLength")]
+    partial class increaseProductImageNameLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -208,7 +210,8 @@ namespace TacticalShop.Backend.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("ProductImageName")
-                        .HasColumnType("varchar(300)");
+                        .HasMaxLength(50)
+                        .HasColumnType("varchar(50)");
 
                     b.Property<string>("ProductName")
                         .IsRequired()
