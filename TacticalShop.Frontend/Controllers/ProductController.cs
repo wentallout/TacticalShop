7c7f8e3924eed6bea6cbcfe336bf2a871f9c3590
Nguyen Dang Khoa <wentallout@gmail.com>
2021-04-07 18:01:36 +0700

    Add Controller for Product Detail

---
+++ TacticalShop.Frontend/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TacticalShop.Frontend.Services;
+
+namespace TacticalShop.Frontend.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductApiClient _productApiClient;
+        public ProductController(IProductApiClient productApiClient)
+        {
+            _productApiClient = productApiClient;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ProductDetail(int id)
+        {
+            var product = await _productApiClient.GetProduct(id);
+            return View(product);
+        }
+
+    }
+}
