d4a3cc38d3e4c26b4e73602a57085711cca709e4
Nguyen Dang Khoa <wentallout@gmail.com>
2021-04-13 22:40:07 +0700

    Add Modal ViewComponent to have product quickview feature

---
+++ TacticalShop.Frontend/Components/ModalViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TacticalShop.Frontend.Services;
+
+namespace TacticalShop.Frontend.Components
+{
+    public class ModalViewComponent : ViewComponent
+    {
+        private readonly IProductApiClient _productApiClient;
+
+        public ModalViewComponent(IProductApiClient productApiClient)
+        {
+            _productApiClient = productApiClient;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var product = await _productApiClient.GetProduct(id);
+            return View(product);
+        }
+    }
+}
