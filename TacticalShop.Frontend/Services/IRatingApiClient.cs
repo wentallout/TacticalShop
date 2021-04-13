7bef38b1a95b467bba2ed3632189221900ac48c9
Nguyen Dang Khoa <wentallout@gmail.com>
2021-04-13 15:59:38 +0700

    Add API client for Rating

---
+++ TacticalShop.Frontend/Services/IRatingApiClient.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using TacticalShop.ViewModels;
+
+namespace TacticalShop.Frontend.Services
+{
+    public interface IRatingApiClient
+    {
+        Task<RatingVm> GetRating(string userid, int productid);
+        Task<RatingCreateRequest> CreateRating(RatingCreateRequest ratingCreateRequest);
+        Task<RatingUpdateRequest> UpdateRating(RatingUpdateRequest ratingUpdateRequest);
+    }
+}
---
+++ TacticalShop.Frontend/Services/RatingApiClient.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TacticalShop.ViewModels;
+
+namespace TacticalShop.Frontend.Services
+{
+    public class RatingApiClient : IRatingApiClient
+    {
+        private readonly HttpClient _httpClient;
+        public RatingApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<RatingVm> GetRating(string userid, int productid)
+        {
+            var response = await _httpClient.GetAsync("api/Ratings/" + userid.ToString() + "/" + productid.ToString());
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsAsync<RatingVm>();
+        }
+        public async Task<RatingCreateRequest> CreateRating(RatingCreateRequest ratingCreateRequest)
+        {
+            HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(ratingCreateRequest), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("api/Ratings", httpContent);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsAsync<RatingCreateRequest>();
+        }
+        public async Task<RatingUpdateRequest> UpdateRating(RatingUpdateRequest ratingUpdateRequest)
+        {
+            HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(ratingUpdateRequest), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync("api/Ratings", httpContent);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsAsync<RatingUpdateRequest>();
+        }
+    }
+}
