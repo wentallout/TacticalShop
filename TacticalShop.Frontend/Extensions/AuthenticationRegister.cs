9e645c9bf122e50ef212be1ebb74c8642a88a9cf
Nguyen Dang Khoa <wentallout@gmail.com>
2021-04-15 10:08:22 +0700

    Add Authentication to Extensions folder

---
+++ TacticalShop.Frontend/Extensions/AuthenticationRegister.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TacticalShop.Frontend.Extensions
+{
+    public static class AuthenticationRegister
+    {
+        public static void AddAuthenticationCustom(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = "Cookies";
+                    options.DefaultChallengeScheme = "oidc";
+                })
+                .AddCookie("Cookies")
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.Authority = configuration.GetServiceUri("backend").ToString();
+
+                    options.RequireHttpsMetadata = false;
+                    options.GetClaimsFromUserInfoEndpoint = true;
+
+                    options.ClientId = "mvc";
+                    options.ClientSecret = "secret";
+                    options.ResponseType = "code";
+
+                    options.SaveTokens = true;
+
+                    options.Scope.Add("openid");
+                    options.Scope.Add("profile");
+                    options.Scope.Add("tacticalshop.api");
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = "name",
+                        RoleClaimType = "role"
+                    };
+                });
+        }
+    }
+}
